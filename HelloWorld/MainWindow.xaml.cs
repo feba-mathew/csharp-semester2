00667d08848ecbaf3e9c18077e6ff72e701bd784
Feba Mariya Varghese <febamaria14@gmail.com>
2020-04-04 13:15:46 -0500

    Homework 1

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Homework 1
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HelloWorld
 {
@@ -24,12 +14,25 @@ namespace HelloWorld
         {
             InitializeComponent();
             //Ex:1
-            WindowState= WindowState.Maximized; 
+            WindowState = WindowState.Maximized;
+            uxName.Focus();
         }
 
         private void uxSubmit_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void dataChanged(object sender, EventArgs e)
+        {
+            if (uxName.Text.Length == 0 || uxPassword.Text.Length == 0)
+            {
+                uxSubmit.IsEnabled = false;
+            }
+            else if (uxName.Text.Length != 0 && uxPassword.Text.Length != 0)
+            {
+                uxSubmit.IsEnabled = true;
+            }
         }
     }
 }
