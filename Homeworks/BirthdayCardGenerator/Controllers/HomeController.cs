d6b7c4524178cc447777642781c5c8675f520b61
Feba Mariya Varghese <febamaria14@gmail.com>
2020-06-01 21:04:40 -0500

    Birthday Card generator assignment

---
+++ Homeworks/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CardForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CardForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
